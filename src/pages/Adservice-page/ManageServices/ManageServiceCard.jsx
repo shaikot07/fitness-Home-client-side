7e7dcae8dd64aac4e3d2f7edd199246e32ce3d02
shaikot07 <saifulislamshaikat007@gmail.com>
2023-11-07 18:42:58 +0600

    set delet function

--- src/pages/Adservice-page/ManageServices/ManageServiceCard.jsx
+++ src/pages/Adservice-page/ManageServices/ManageServiceCard.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
+import Swal from 'sweetalert2';
 
-const ManageServicCard = ({service}) => {
+const ManageServicCard = ({service,setControl,control}) => {
+      const {user}=useContext(AuthContext)
       const {_id,img,area,description,email,price,providerName,serviceName}=service ||{}
+
+
+      const handleDelete = (_id) => {
+            console.log(_id);
+            Swal.fire({
+                  title: 'Are you sure?',
+                  text: "You won't be able to revert this!",
+                  icon: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#3085d6',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                  if (result.isConfirmed) {
+
+                        console.log('delete confrm');
+                        fetch(`http://localhost:5000/newservices/${_id}`, {
+                              method: 'DELETE'
+                        })
+                              .then(res => res.json())
+                              .then(data => {
+                                    if (data.deletedCount > 0) {
+                                          Swal.fire(
+                                                'Deleted!',
+                                                'service has been deleted.',
+                                                'success'
+                                          )
+                                          
+                                          // const remaining =products.filter(item =>item._id !== _id);
+                                          // // console.log(remaining);
+                                          // setProducts(remaining)
+                                    }
+                                    setControl(!control)
+                              }).catch(error => {
+                                    console.log(error);
+                              })
+                  }
+            })
+      }
       return (
             
-                  <tr>
+                  <tr className=''>
                         <th>
-                              {/* <button onClick={()=>handleDelete(_id)} className="btn btn-circle  btn-sm btn-outline">
+                              <button onClick={()=>handleDelete(_id)} className="btn btn-circle  btn-sm btn-outline">
                               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                        </button> */}
+                        </button>
                               <button>delete</button>
                         </th>
                         <td>
@@ -17,19 +59,96 @@ const ManageServicCard = ({service}) => {
                                          <img src={img} alt="" />
                                     </div>
                               </div>
-                              <div>
-                                    <div className="font-bold">email</div>
-                                    <div className="text-sm opacity-50">United States</div>
-                              </div>
+                              
 
                         </td>
                         <td>
                              {serviceName}
                         </td>
                         <td>{description}</td>
-                        <td>$</td>
+                        <td>${price}</td>
                         <th>
-                              <button>Update</button>
+                        <div className='w-full mx-auto mt-5 mb-0 text-center'>
+                                    {/* Open the modal using document.getElementById('ID').showModal() method */}
+                                    <button className="btn bg-[#E31C25] text-white  hover:bg-slate-950" onClick={() => document.getElementById('my_modal_5').showModal()}>Edit Info</button>
+                                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                                          <div className="modal-box">
+                                                <h3 className="font-bold text-lg">Hello!</h3>
+                                                {/* <p className="py-4">Press ESC key or click the button below to close</p> */}
+                                                <div className="flex">
+                                                      <form  method="dialog">
+
+                                                            <div className='grid grid-cols-2 gap-2'>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">Service Image Url</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="text" placeholder="" defaultValue={img} name='img' className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">Service name</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="text" placeholder="" defaultValue={serviceName} name='serviceName' className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">User Email</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="text" placeholder="" readOnly value={user?.email} name='email' className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">Area</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="area" placeholder=""
+                                                                                    name='area' defaultValue={area} className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">Description</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="text"
+                                                                                    name='description' defaultValue={description} placeholder="Description" className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  <div>
+                                                                        <label className="label">
+                                                                              <span className="label-text">Price</span>
+                                                                        </label>
+                                                                        <label className="input-group input-group-vertical">
+
+                                                                              <input type="text" placeholder="" defaultValue={`$${price}`} name='price' className="input input-bordered" />
+                                                                        </label>
+                                                                  </div>
+                                                                  
+                                                            </div>
+
+                                                            {/* if there is a button in form, it will close the modal */}
+                                                            <div className='flex gap-6 mt-3 justify-center'>
+                                                                  <button className="btn btn-outline">Close</button>
+                                                                  <button type='submit' className='bg-[#E31C25] py-2 px-3 text-white rounded-lg hover:bg-slate-950'>Update</button>
+                                                            </div>
+                                                      </form>
+                                                </div>
+                                          </div>
+                                    </dialog>
+                              </div>
+                              
                         </th>
                   </tr>
             
--- src/pages/Adservice-page/ManageServices/ManageServices.jsx
+++ src/pages/Adservice-page/ManageServices/ManageServices.jsx
@@ -8,6 +8,7 @@ const ManageServices = () => {
       // console.log(user);
       const [loading,setLoading]=useState(true)
       const [servicess,setServicess] = useState([]);
+      const [control,setControl]=useState(true)
       const url = `http://localhost:5000/newservices?email=${user.email}`;
       useEffect(() => {
             axios.get(url,{withCredentials:true})
@@ -26,7 +27,7 @@ const ManageServices = () => {
             //       })
                   
                  
-      }, [url])
+      }, [url,control])
       console.log(servicess);
       if (loading) {
             return <div className='max-w-6xl mx-[300px] mt-10 mb-10'><div className="w-16 h-16  border-4 border-dashed rounded-full animate-spin border-violet-400"></div></div>
@@ -41,24 +42,26 @@ const ManageServices = () => {
                               <thead>
                                     <tr>
                                           <th>
-                                                <label>
+                                                {/* <label>
                                                       <input type="checkbox" className="checkbox" />
-                                                </label>
+                                                </label> */}
                                           </th>
                                           <th>image</th>
-                                          <th>service</th>
-                                          <th>date</th>
-                                          <th>price</th>
-                                          <th>status</th>
+                                          <th>Service Name</th>
+                                          <th>Description</th>
+                                          <th>Price</th>
+                                          <th></th>
                                     </tr>
                               </thead>
-                              <tbody>
+                              <tbody className='mb-5'>
                                    
                               
                               {
                                     servicess.map(service =><ManageServicCard 
                                           key={service._id}
                                           service={service}
+                                          setControl={setControl}
+                                          control={control}
                                     ></ManageServicCard>)
                               }
                               
